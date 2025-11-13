ceb161d4588ef6f8409263286e9907fe1953ccd7
Enes <enesalkus@hotmail.com>
2025-11-13 01:57:50 +0100

    Task 3 : Generics

--- src/main/java/pl/merito/quizsystem/App.java
+++ src/main/java/pl/merito/quizsystem/App.java
@@ -1,8 +1,10 @@
 package pl.merito.quizsystem;
 
 import pl.merito.quizsystem.interfaces.IAnswer;
+import pl.merito.quizsystem.interfaces.IQuestion;
 import pl.merito.quizsystem.model.Answer;
 import pl.merito.quizsystem.model.Question;
+import pl.merito.quizsystem.model.Quiz;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,23 +13,6 @@ import java.util.Scanner;
 public class App
 {
     public static void main(String[] args) {
-        List<IAnswer> answers = new ArrayList<IAnswer>();
 
-        answers.add(new Answer("Tarantino", false));
-        answers.add(new Answer("Nolan", true));
-        answers.add(new Answer("Cameron", false));
-        answers.add(new Answer("Caglar", false));
-
-        Question question = new Question("Who is the director of the film Interstellar?", answers);
-
-        System.out.println(question);
-        System.out.print("Enter your answer : ");
-
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-
-        System.out.println("Your answer is " + (question.checkAnswer(input) ? "correct!" : "incorrect!"));
-
-        sc.close();
     }
 }
